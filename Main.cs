2efa36992493c84f7de34c17289b23f93fb53aaa
Repsi0 <repeerc710@gmail.com>
2020-04-14 20:12:10 -0500

    Update Main.cs
    Moved some stuff out of Main.cs; add more to it.

--- Main.cs
+++ Main.cs
@@ -12,15 +12,25 @@ public class MoreComponents : Mod {
     public override string Author => "Repsi0";
     public override Version ModVersion => new Version("0.1");
 
-    private static string listeningChars_alpha = "abcdefghijklmnopqrstuvwxyz";
-    private static string listeningKeys_space = "space";
-    private static string listeningChars_num = "1234567890";
-
     public static bool[] alphakey_data;
     public static bool[] numpad_data;
+    public static bool[] wasd_data;
+    public static bool[] arrowkey_data;
+    public static bool spacebar_data;
+    public static bool[] controlkey_data;
+    public static bool[] shiftkey_data;
+    public static bool[] altkey_data;
+    public static bool[] enterkey_data;
 
     public static List<KeyboardHandler> keyboards;
     public static List<NumpadHandler> numpads;
+    public static List<ArrowKeyHandler> arrowkeys;
+    public static List<WASDHandler> wasds;
+    public static List<SpacebarHandler> spacebars;
+    public static List<ControlKeyHandler> controlkeys;
+    public static List<ShiftKeyHandler> shiftkeys;
+    public static List<AltKeyHandler> altkeys;
+    public static List<EnterKeyHandler> enterkeys;
 
     public static void onKeyboardUpdate() {
         alphakey_data[0]=KBR.isActive("a")|KBR.isActive("c")|KBR.isActive("e")|KBR.isActive("g")|
@@ -46,8 +56,40 @@ public class MoreComponents : Mod {
         numpad_data[2]=KBR.isActive("4")|KBR.isActive("5")|KBR.isActive("6")|KBR.isActive("7");
         numpad_data[3]=KBR.isActive("8")|KBR.isActive("9")|KBR.isActive("0");
 
+        wasd_data[0]=KBR.isActive("w");
+        wasd_data[1]=KBR.isActive("a");
+        wasd_data[2]=KBR.isActive("s");
+        wasd_data[3]=KBR.isActive("d");
+
+        arrowkey_data[0]=KBR.isActive("up");
+        arrowkey_data[1]=KBR.isActive("left");
+        arrowkey_data[2]=KBR.isActive("down");
+        arrowkey_data[3]=KBR.isActive("right");
+
+        spacebar_data=KBR.isActive("space");
+
+        controlkey_data[0]=KBR.isActive("left ctrl");
+        controlkey_data[1]=KBR.isActive("right ctrl");
+
+        shiftkey_data[0]=KBR.isActive("left shift");
+        shiftkey_data[1]=KBR.isActive("right shift");
+
+        altkey_data[0]=KBR.isActive("left alt");
+        altkey_data[1]=KBR.isActive("right alt");
+
+        enterkey_data[0]=KBR.isActive("return");
+        enterkey_data[1]=KBR.isActive("enter");
+
         List<KeyboardHandler> kbhs_to_remove = new List<KeyboardHandler>();
         List<NumpadHandler> nps_to_remove = new List<NumpadHandler>();
+        List<ArrowKeyHandler> arrowkeys_to_remove = new List<ArrowKeyHandler>();
+        List<WASDHandler> wasds_to_remove = new List<WASDHandler>();
+        List<SpacebarHandler> spacebars_to_remove = new List<SpacebarHandler>();
+        List<ControlKeyHandler> ctrl_to_remove = new List<ControlKeyHandler>();
+        List<ShiftKeyHandler> shift_to_remove = new List<ShiftKeyHandler>();
+        List<AltKeyHandler> aks_to_remove = new List<AltKeyHandler>();
+        List<EnterKeyHandler> eks_to_remove = new List<EnterKeyHandler>();
+
         foreach(KeyboardHandler kbh in keyboards) {
             if(kbh != null && kbh.isActiveAndEnabled) {
                 kbh.QueueCircuitLogicUpdate();
@@ -55,11 +97,60 @@ public class MoreComponents : Mod {
                 kbhs_to_remove.Add(kbh);
             }
         }
-        foreach(NumpadHandler np in numpads) {
-            if(np!=null&&np.isActiveAndEnabled) {
-                np.QueueCircuitLogicUpdate();
+        foreach(NumpadHandler handler in numpads) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
             } else {
-                nps_to_remove.Add(np);
+                nps_to_remove.Add(handler);
+            }
+        }
+        foreach(ArrowKeyHandler handler in arrowkeys) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                arrowkeys_to_remove.Add(handler);
+            }
+        }
+        foreach(WASDHandler handler in wasds) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                wasds_to_remove.Add(handler);
+            }
+        }
+        foreach(SpacebarHandler handler in spacebars) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                spacebars_to_remove.Add(handler);
+            }
+        }
+        foreach(ControlKeyHandler handler in controlkeys) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                ctrl_to_remove.Add(handler);
+            }
+        }
+        foreach(ShiftKeyHandler handler in shiftkeys) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                shift_to_remove.Add(handler);
+            }
+        }
+        foreach(AltKeyHandler handler in altkeys) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                aks_to_remove.Add(handler);
+            }
+        }
+        foreach(EnterKeyHandler handler in enterkeys) {
+            if(handler!=null&&handler.isActiveAndEnabled) {
+                handler.QueueCircuitLogicUpdate();
+            } else {
+                eks_to_remove.Add(handler);
             }
         }
 
@@ -69,259 +160,77 @@ public class MoreComponents : Mod {
         foreach(NumpadHandler np in nps_to_remove) {
             numpads.Remove(np);
         }
+        foreach(ArrowKeyHandler ak in arrowkeys_to_remove) {
+            arrowkeys.Remove(ak);
+        }
+        foreach(WASDHandler ak in wasds_to_remove) {
+            wasds.Remove(ak);
+        }
+        foreach(SpacebarHandler ak in spacebars_to_remove) {
+            spacebars.Remove(ak);
+        }
+        foreach(ControlKeyHandler handler in ctrl_to_remove) {
+            controlkeys.Remove(handler);
+        }
+        foreach(ShiftKeyHandler handler in shift_to_remove) {
+            shiftkeys.Remove(handler);
+        }
+        foreach(AltKeyHandler handler in aks_to_remove) {
+            altkeys.Remove(handler);
+        }
+        foreach(EnterKeyHandler handler in eks_to_remove) {
+            enterkeys.Remove(handler);
+        }
     }
 
     public override void BeforePatch() {
-        //cool misc gates
-        var randomizerShape = PrefabBuilder.Cube
-                         .WithIO(CubeSide.Top, SideType.Input)
-                         .WithIO(CubeSide.Front, SideType.Output)
-                         .WithColor(new UnityEngine.Color(1.0f, 0.553f, 0.796f));
-        //base gates
-        var xorShape = PrefabBuilder.Cube
-                        .WithIO(CubeSide.Top, SideType.Input)
-                        .WithIO(CubeSide.Back, SideType.Input)
-                        .WithIO(CubeSide.Front, SideType.Output)
-                        .WithColor(new UnityEngine.Color(0.280f, 0.161f, 0.586f));
-        var orShape =    PrefabBuilder.Cube
-                        .WithIO(CubeSide.Top, SideType.Input)
-                        .WithIO(CubeSide.Back, SideType.Input)
-                        .WithIO(CubeSide.Front, SideType.Output)
-                        .WithColor(new UnityEngine.Color(0.280f, 0.561f, 0.286f));
-        var andShape =   PrefabBuilder.Cube
-                        .WithIO(CubeSide.Top, SideType.Input)
-                        .WithIO(CubeSide.Back, SideType.Input)
-                        .WithIO(CubeSide.Front, SideType.Output)
-                        .WithColor(new UnityEngine.Color(0.580f, 0.161f, 0.286f));
-        //memory components
-        var dffShape = PrefabBuilder.Cube //D-flip-flop
-                      .WithIO(CubeSide.Left, SideType.Input) //data
-                      .WithIO(CubeSide.Top, SideType.Input) //write
-                      .WithIO(CubeSide.Front, SideType.Output) //out
-                      .WithColor(new UnityEngine.Color(0.827f, 0.467f, 0.0f));
-        var tffShape = PrefabBuilder.Cube //T-flip-flop
-                      .WithIO(CubeSide.Top, SideType.Input) //input
-                      .WithIO(CubeSide.Front, SideType.Output)
-                      .WithColor(new UnityEngine.Color(0.467f, 0.827f, 0.0f));
-        var rsShape = PrefabBuilder.Cube //RS-latch
-                      .WithIO(CubeSide.Back, SideType.Input) //set
-                      .WithIO(CubeSide.Top, SideType.Input) //reset
-                      .WithIO(CubeSide.Front, SideType.Output)
-                      .WithColor(new UnityEngine.Color(0.467f, 0.0f, 0.827f));
-        var regShape = PrefabBuilder.Cube //Register (DFF + read)
-                      .WithIO(CubeSide.Left, SideType.Input) //data
-                      .WithIO(CubeSide.Top, SideType.Input) //write
-                      .WithIO(CubeSide.Back, SideType.Input) //read
-                      .WithIO(CubeSide.Front, SideType.Output) //output
-                      .WithColor(new UnityEngine.Color(0.827f, 0.467f, 0.0f));
-        //math
-        var addShape = PrefabBuilder.Cube //Full adder
-                      .WithIO(CubeSide.Left, SideType.Input) //A
-                      .WithIO(CubeSide.Right, SideType.Input) //B
-                      .WithIO(CubeSide.Back, SideType.Input) //cin
-                      .WithIO(CubeSide.Top, SideType.Output) //cout
-                      .WithIO(CubeSide.Front, SideType.Output) //output
-                      .WithColor(new UnityEngine.Color(0.827f, 0.0f, 0.232f));
-        //io
-        //0.596f, 0.424f, 0.263f
-        var alphaKeyShape = PrefabBuilder.Cube //alphabet key
-                           .WithIO(CubeSide.Left, SideType.Output) //1
-                           .WithIO(CubeSide.Front, SideType.Output) //2
-                           .WithIO(CubeSide.Right, SideType.Output) //4
-                           .WithIO(CubeSide.Back, SideType.Output) //8
-                           .WithIO(CubeSide.Top, SideType.Output) //16
-                           .WithColor(new UnityEngine.Color(0.596f, 0.424f, 0.263f));
-        var numberKeyShape = PrefabBuilder.Cube //number key
-                            .WithIO(CubeSide.Left, SideType.Output) //1
-                            .WithIO(CubeSide.Front, SideType.Output) //2
-                            .WithIO(CubeSide.Right, SideType.Output) //4
-                            .WithIO(CubeSide.Back, SideType.Output) //8
-                            .WithColor(new UnityEngine.Color(0.424f, 0.424f, 0.596f));
-        var keyInterfaceShape = PrefabBuilder.Cube //allows player to send a signal to it to listen to keys
-                               .WithIO(CubeSide.Top, SideType.Input)
-                               .WithColor(new UnityEngine.Color(0.424f, 0.596f, 0.424f));
-        //false/true
-        var falseTrueShape = PrefabBuilder.Cube
-                        .WithIO(CubeSide.Front, SideType.Output);
+        ComponentRegistrar.RegisterComponents();
 
-        //debug
-        var debugShape = PrefabBuilder.Cube //Debug Component
-                        .WithIO(CubeSide.Back, SideType.Input)
-                        .WithIO(CubeSide.Left, SideType.Input)
-                        .WithIO(CubeSide.Front, SideType.Output)
-                        .WithIO(CubeSide.Right, SideType.Output);
+        alphakey_data = new bool[5];
+        numpad_data=new bool[4];
+        arrowkey_data=new bool[4];
+        wasd_data=new bool[4];
+        spacebar_data=new bool();
+        controlkey_data=new bool[2];
+        altkey_data=new bool[2];
+        shiftkey_data=new bool[2];
+        enterkey_data=new bool[2];
+        keyboards=new List<KeyboardHandler>();
+        numpads=new List<NumpadHandler>();
+        arrowkeys=new List<ArrowKeyHandler>();
+        wasds=new List<WASDHandler>();
+        spacebars=new List<SpacebarHandler>();
+        controlkeys=new List<ControlKeyHandler>();
+        altkeys=new List<AltKeyHandler>();
+        shiftkeys=new List<ShiftKeyHandler>();
+        enterkeys=new List<EnterKeyHandler>();
+        RegisterAllKeys();
+    }
 
-        ComponentRegistry.CreateNew<RandomizerHandler>("randomizer", "Randomizer", randomizerShape);
+    private const string listeningChars_alpha = "abcdefghijklmnopqrstuvwxyz";
+    private const string listeningKeys_space = "space";
+    private const string listeningChars_num = "1234567890";
 
-        ComponentRegistry.CreateNew<XORHandler>("xor", "XOR", xorShape);
-        ComponentRegistry.CreateNew<ORHandler>("or", "OR", orShape);
-        ComponentRegistry.CreateNew<ANDHandler>("and", "AND", andShape);
-
-        ComponentRegistry.CreateNew<DFFHandler>("dff", "D-Flip-Flop", dffShape);
-        ComponentRegistry.CreateNew<TFFHandler>("tff", "T-Flip-Flop", tffShape);
-        ComponentRegistry.CreateNew<RSHandler>("rsnor", "RS-Latch", rsShape);
-        ComponentRegistry.CreateNew<RegHandler>("reg", "Register", regShape);
-
-        ComponentRegistry.CreateNew<KeyboardListenerHandler>("keylistener", "Keyboard Interface", keyInterfaceShape);
-        ComponentRegistry.CreateNew<KeyboardHandler>("alphakey", "Keyboard", alphaKeyShape);
-        ComponentRegistry.CreateNew<NumpadHandler>("numkey", "Numpad", numberKeyShape);
-
-        ComponentRegistry.CreateNew<TrueHandler>("truegate", "TRUE", falseTrueShape);
-        ComponentRegistry.CreateNew<FalseHandler>("falsegate", "FALSE", falseTrueShape);
-
-        ComponentRegistry.CreateNew<AddHandler>("add", "Adder", addShape);
-
-        //debug component
-        //ComponentRegistry.CreateNew<DebugHandler>("debug", "MY ONLY PURPOSE IS FOR DEBUGGING PLEASE END MY PAINFUL EXISTENCE BY COMMENTING OUT MY CODE, REPSI", debugShape);
-
+    public static void RegisterAllKeys() {
         //register keys this is the worst ever why did i do it like this
-        for(int i = 0; i < listeningChars_alpha.Length; i++) {
+        for(int i = 0;i<listeningChars_alpha.Length;i++) {
             KBR.registerKey(""+listeningChars_alpha[i]);
         }
         KBR.registerKey(listeningKeys_space);
         for(int i = 0;i<listeningChars_num.Length;i++) {
             KBR.registerKey(""+listeningChars_num[i]);
         }
-
-        alphakey_data = new bool[5];
-        numpad_data=new bool[4];
-        keyboards=new List<KeyboardHandler>();
-        numpads=new List<NumpadHandler>();
-}
-}
-public class DebugHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        Outputs[0].On=Inputs[0].On;
-        Outputs[1].On=Inputs[1].On;
-    }
-}
-public class TrueHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        Outputs[0].On=true;
-    }
-}
-public class FalseHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        Outputs[0].On=false;
-    }
-}
-public class KeyboardListenerHandler : UpdateHandler {
-    bool lastState = false;
-    protected override void CircuitLogicUpdate() {
-        if(Inputs[0].On&&!lastState) {
-            KBR.processKeys();
-            MoreComponents.onKeyboardUpdate();
-        }
-        lastState=Inputs[0].On;
-    }
-}
-public class KeyboardHandler : UpdateHandler {
-    public KeyboardHandler() {
-        MoreComponents.keyboards.Add(this);
-    }
-    protected override void CircuitLogicUpdate() {
-        //ABCDEFGHIJKLMNOPQRSTUVWXYZ
-        
-        for(int i = 0; i < Outputs.Length; i++) {
-            Outputs[i].On=MoreComponents.alphakey_data[i];
-            IGConsole.Log(Outputs[i].On?1:0);
-        }
-    }
-}
-public class NumpadHandler : UpdateHandler {
-    public NumpadHandler() {
-        MoreComponents.numpads.Add(this);
-    }
-    
-    protected override void CircuitLogicUpdate() {
-        //0123456789
-        for(int i = 0;i<Outputs.Length;i++) {
-            Outputs[i].On=MoreComponents.numpad_data[i];
-        }
-    }
-}
-public class AddHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        bool firstLayerCout = Inputs[0].On&Inputs[1].On; //A & B
-        bool firstAdd = Inputs[0].On^Inputs[1].On; //A XOR B
-        bool secondLayerCout = firstAdd&Inputs[2].On; //XOR & Cin
-        bool secondAdd = firstAdd^Inputs[2].On; //XOR XOR Cin
-        Outputs[0].On=secondLayerCout|firstLayerCout;
-        Outputs[1].On=secondAdd;
-    }
-}
-public class RegHandler : UpdateHandler {
-    [SaveThisAttribute]
-    bool data;
-    protected override void CircuitLogicUpdate() {
-        if(Inputs[1].On) data=Inputs[0].On; //if writing
-        if(Inputs[2].On) Outputs[0].On=data; //if reading
-        else Outputs[0].On=false; //if not reading don't output lol
-    }
-}
-public class DFFHandler : UpdateHandler {
-    [SaveThisAttribute]
-    bool data;
-    protected override void CircuitLogicUpdate() {
-        if(Inputs[1].On) data=Inputs[0].On; //set internal data to input if writing
-        Outputs[0].On=data; //always = data
-    }
-}
-public class TFFHandler : UpdateHandler {
-    [SaveThisAttribute]
-    bool lastState = false; //last state of the input
-    protected override void CircuitLogicUpdate() {
-        bool changedStateToTrue=(lastState==false)&&(Inputs[0]==true); //check if state has changed to true
-        if(changedStateToTrue) { //if so,
-            Outputs[0].On=!Outputs[0].On; //toggle output
-        }
-        lastState=Inputs[0].On; //set 
-    }
-}
-public class RSHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        if(Inputs[1].On) Outputs[0].On=true; //set
-        if(Inputs[0].On) Outputs[0].On=false; //reset //putting [0] check last b/c i'd prefer if both are on, to ultimately output false
-    }
-}
-public class XORHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        this.Outputs[0].On=this.Inputs[1].On^this.Inputs[0].On; //xor
-    }
-}
-public class ORHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        this.Outputs[0].On=this.Inputs[1].On|this.Inputs[0].On; //or
-    }
-}
-public class ANDHandler : UpdateHandler {
-    protected override void CircuitLogicUpdate() {
-        this.Outputs[0].On=this.Inputs[1].On&this.Inputs[0].On; //and
-    }
-}
-public class RandomizerHandler : UpdateHandler {
-    [SaveThisAttribute]
-    bool lastState; //save last input
-    [SaveThisAttribute]
-    bool previousOutput; //save previous output so it doesn't immediately default off the next tick
-    System.Random r = null;
-    protected override void CircuitLogicUpdate() {
-        bool currentState = this.Inputs[0].On;
-        bool changedState = (currentState!=lastState);
-        bool wasFalseNowTrue = changedState&!lastState;
-        bool notChangedStillTrue = lastState&currentState;
-
-        if(wasFalseNowTrue) { //if the gate is now active
-            //randomize
-            if(r == null) r=new System.Random();
-            previousOutput=r.Next(2)==1; //change previous output
-        }
-        if(currentState)
-            //set to output or previous output, whichever's more recent
-            this.Outputs[0].On=previousOutput;
-        else
-            //just set it to false
-            this.Outputs[0].On=false;
-        lastState=currentState;
+        KBR.registerKey("up");
+        KBR.registerKey("left");
+        KBR.registerKey("down");
+        KBR.registerKey("right");
+        KBR.registerKey("left ctrl");
+        KBR.registerKey("right ctrl");
+        KBR.registerKey("left shift");
+        KBR.registerKey("right shift");
+        KBR.registerKey("left alt");
+        KBR.registerKey("right alt");
+        KBR.registerKey("enter");
+        KBR.registerKey("return");
     }
 }
